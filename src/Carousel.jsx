eaef70816f5b6eff1bb9b1a3a492f0336a547492
dustin <dustinusey@me.com>
2024-07-25 14:22:49 -0500

    added sup copy

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -14,7 +14,24 @@ const Carousel = () => {
     >
       {/* screen */}
       <div className="h-full w-full rounded-md bg-zinc-200 flex flex-col overflow-hidden">
-        <ViewPlayer />
+        {!mobileCarousel ? (
+          <div className="w-full flex justify-between items-center">
+            <div className="w-1/2">
+              <ViewPlayer />
+            </div>
+            <div className="w-1/2 flex flex-col justify-center items-center gap-3 mb-auto mt-12 text-center">
+              <h2 className="text-2xl font-bold">Lorem ipsum dolor sit!</h2>
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro,
+                voluptas.
+              </p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+          </div>
+        ) : (
+          <ViewPlayer />
+        )}
       </div>
       {/* controls */}
       <div
